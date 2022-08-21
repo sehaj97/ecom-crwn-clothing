01e29e219780144d3d4f58c094a11666b517c0c5
Sehajpreet Singh <sehajmagan@gmail.com>
2022-08-21 17:43:44 -0400

    added routing to handle redirects to new page and added nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 function Shop() {
   return <h1>I am shop</h1>;
 }
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
